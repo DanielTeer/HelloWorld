559096c579d3614a0dabb67fd397a92d0868e432
DanielTeer <daniel.teer94@gmail.com>
2026-05-16 22:25:23 -0700

    Initial commit

---
+++ Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Hello Everyone");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Hello World");
+
+    }
+}
